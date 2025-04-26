f5471c7b3f4f0eb04b27f1038336f9bf9be35c3c
thesaifadnan <saifadnansheikh@gmail.com>
2025-04-26 16:25:14 +0530

    Initial frontend login/signup setup

---
+++ frontend/src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Skillswap</h1>
+      <Login />
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
